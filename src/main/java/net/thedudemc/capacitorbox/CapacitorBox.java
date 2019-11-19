6c8ca78027fa3b8a9c7efac2ff51d4b1b57b70f7
jmilthedude <jmiltheman@gmail.com>
2019-11-19 10:24:57 -0600

    Updated texture and lang files

--- src/main/java/net/thedudemc/capacitorbox/CapacitorBox.java
+++ src/main/java/net/thedudemc/capacitorbox/CapacitorBox.java
@@ -17,7 +17,7 @@ public class CapacitorBox {
 
 	public static final String MODID = "capacitorbox";
 	public static final String NAME = "Capacitor Box";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.12.2-1.0.1";
 
 	private static Logger logger = LogManager.getLogger(CapacitorBox.NAME);
 
